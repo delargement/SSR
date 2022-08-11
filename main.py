04a2e9be70f864d3918cef643989f84bbc9735d9
delargement <ooh.nooess@gmail.com>
2022-08-11 12:16:32 +0800

    rssgen

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
 import trafilatura
+import dict2xml
 import feedparser
 import yaml
+from feedgen.feed import FeedGenerator
 
 
 def readfeeds():
     with open("feeds.yaml", "r") as f:
-        feeds = yaml.load(f)
+        feeds = yaml.load(f,yaml.SafeLoader)
         return feeds
 
+def genfeed(feed):
+    fg = FeedGenerator()
+    fg.id(feed['feed']['link'])
+    fg.title(feed['feed']['title'])
+    fg.link(href=feed['feed']['link'])
+    fg.updated(feed['feed']['updated'])
+    fg.description(feed['feed']['title'])
+    for entry in feed['entries']:
+        fe = fg.add_entry()
+        fe.id(entry['id'])
+        fe.title(entry['title'])
+        fe.link(href=entry['link'])
+        fe.updated(entry['updated'])
+        fe.content(entry['content'], type='xhtml')
+        fe.updated(entry['updated'])
+        fe.summary(entry['summary'], type='html')
+        fe.description(entry['title'])
+        # fe.author({'name': auth['name'] for auth in entry['authors']})
+    fg.rss_file('feed.xml')
+    return fg.rss_str()
 
 def reconstructfeed(feedurl):
     xml = feedparser.parse(feedurl)
@@ -19,15 +41,14 @@ def reconstructfeed(feedurl):
                 for ls in links]
     for entry, content in zip(xml.entries, contents):
         entry['content'] = content
-    return xml
+    # return xml
+    return genfeed(xml)
+    # return dict2xml.dict2xml(xml)
 
 
 def main(key):
     feeds = readfeeds()
     for feed in feeds:
-        if key == feed.key:
-            url = feed.url
+        if key == feed['key']:
+            url = feed['feedurl']
             return reconstructfeed(url)
-
-
-print(reconstructfeed('https://www.theatlantic.com/feed/all/'))
--- server.py
+++ server.py
@@ -1,9 +1,16 @@
-from flask import Flask,request
+from flask import Flask, request
 from main import main
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
     key = request.args.get('key')
+    if key is None:
+        return 'Missing key'
+    print(key)
     return main(key)
+
+
+app.run(debug=True, port=5000)
